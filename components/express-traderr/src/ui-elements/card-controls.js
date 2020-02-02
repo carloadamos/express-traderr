2ed75e0f68d5ff9412367f68fa1b8b10ec8b1c36
carlo <johncarloadamos@gmail.com>
2020-02-02 15:08:13 +0800

    Add UI behavior to controls.

--- components/express-traderr/src/ui-elements/card-controls.js
+++ components/express-traderr/src/ui-elements/card-controls.js
@@ -9,6 +9,30 @@ export class CaadControls extends LitElement {
   static get properties() {
     return {
       /**
+       * Disable stock code field.
+       * @type {boolean}
+       */
+      _disableStockField: {
+        type: Boolean,
+      },
+
+      /**
+       * Disable to date field.
+       * @type {boolean}
+       */
+      _disableToDatePicker: {
+        type: Boolean,
+      },
+
+      /**
+       * From date.
+       * @type {string}
+       */
+      _fromDate: {
+        type: String,
+      },
+
+      /**
        * Flag for disabling all textbox.
        * @type {boolean}
        */
@@ -23,14 +47,35 @@ export class CaadControls extends LitElement {
       _isSingleDate: {
         type: Boolean,
       },
+
+      /**
+       * Stock code.
+       * @type {string}
+       */
+      _stockCode: {
+        type: String,
+      },
+
+      /**
+       * To date.
+       * @type {string}
+       */
+      _toDate: {
+        type: String,
+      },
     };
   }
 
   constructor() {
     super();
 
+    this._disableStockField = false;
+    this._disableToDatePicker = false;
+    this._fromDate = '';
     this._isAllStockChecked = false;
     this._isSingleDate = false;
+    this._stockCode = '';
+    this._toDate = '';
   }
 
   static get styles() {
@@ -51,29 +96,88 @@ export class CaadControls extends LitElement {
 
   render() {
     return html`
-      <vaadin-text-field .disabled=${this._isAllStockChecked} placeholder="ABCDE" label="Stock">
+      <vaadin-text-field
+        value="${this._stockCode}"
+        .disabled=${this._isAllStockChecked}
+        placeholder="ABCDE"
+        label="Stock"
+        @change="${this._updateStockCode}"
+      >
       </vaadin-text-field>
-      <vaadin-checkbox @change="${this._disableStockText}"> </vaadin-checkbox>
+      <vaadin-checkbox @change="${this._disableStockText}" ?checked=${this._disableStockField}>
+      </vaadin-checkbox>
 
-      <vaadin-date-picker label="From date:" placeholder="Pick a date"></vaadin-date-picker>
       <vaadin-date-picker
+        value="${this._fromDate}"
+        label="From date:"
+        placeholder="Pick a date"
+        @value-changed="${this._updateFromDate}"
+      ></vaadin-date-picker>
+
+      <vaadin-date-picker
+        value="${this._toDate}"
         label="To date:"
         placeholder="Pick a date"
         .disabled=${this._isSingleDate}
+        @value-changed="${this._updateToDate}"
       ></vaadin-date-picker>
-      <vaadin-checkbox @change="${this._disableToDate}"> </vaadin-checkbox>
+      <vaadin-checkbox @change="${this._disableToDate}" ?checked=${this._disableToDatePicker}>
+      </vaadin-checkbox>
 
-      <vaadin-button theme="primary">Save</vaadin-button>
-      <vaadin-button theme="error primary">Remove</vaadin-button>
+      <vaadin-button theme="primary" @click="${this._buttonClicked}">Save</vaadin-button>
+      <vaadin-button theme="error primary" @click="${this._resetForm}">Remove</vaadin-button>
     `;
   }
 
+  /**
+   * Disable stock code UI when selecting all stocks.
+   */
   _disableStockText() {
     this._isAllStockChecked = !this._isAllStockChecked;
+    this._disableStockField = !this._disableStockField;
   }
 
+  /**
+   * Disable `To Date` UI when using one date only.
+   */
   _disableToDate() {
     this._isSingleDate = !this._isSingleDate;
+    this._disableToDatePicker = !this._disableToDatePicker;
+  }
+
+  /**
+   * Reset form fields.
+   */
+  _resetForm() {
+    this._disableStockField = false;
+    this._disableToDatePicker = false;
+    this._fromDate = '';
+    this._stockCode = '';
+    this._toDate = '';
+  }
+
+  /**
+   * Update property when textbox value changed.
+   * @param {Event} e Event value.
+   */
+  _updateStockCode(e) {
+    this._stockCode = e.target.value;
+  }
+
+  /**
+   * Update `From Date` value.
+   * @param {Event} e Event value
+   */
+  _updateFromDate(e) {
+    this._fromDate = e.target.value;
+  }
+
+  /**
+   * Update `To Date` value.
+   * @param {Event} e Event value.
+   */
+  _updateToDate(e) {
+    this._toDate = e.target.value;
   }
 }
 
