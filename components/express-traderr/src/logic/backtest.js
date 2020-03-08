974c17425586a813f751a5da6acbf893e0cd0d64
carloadamos <johncarloadamos@gmail.com>
2020-03-08 19:01:35 +0800

    refactor backtest

--- components/express-traderr/src/logic/backtest.js
+++ components/express-traderr/src/logic/backtest.js
@@ -51,33 +51,14 @@ export default class BackTest {
         case SIGNALS.PRICE_BELOW_SMA: {
           const { periods, source, newProperty } = signal;
 
-          periods.forEach(period => {
-            const sma = new SimpleMovingAverage(this.stockList, period, source, newProperty);
-            this.stockList = sma.compute();
-          });
-
-          if (periods.length >= 2) {
-            /* Always put the slow at the start of the list */
-            this.stockList = SimpleMovingAverage.identifyTrend(
-              this.stockList,
-              periods[0],
-              periods[1],
-            );
-          }
+          this._initSignalsWithSMA(periods, source, newProperty);
           break;
         }
         case SIGNALS.PRICE_ABOVE_EMA:
         case SIGNALS.PRICE_BELOW_EMA: {
           const { periods, source, newProperty, smaSource } = signal;
-          const ema = new ExponentialMovingAverage(
-            this.stockList,
-            periods[0],
-            source,
-            newProperty,
-            smaSource,
-          );
 
-          this.stockList = ema.compute();
+          this._initSignalsWithEMA(periods, source, newProperty, smaSource);
           break;
         }
         case SIGNALS.MACD_CROSSOVER:
@@ -85,14 +66,8 @@ export default class BackTest {
         case SIGNALS.MACD_ABOVE_SIGNAL:
         case SIGNALS.MACD_BELOW_SIGNAL: {
           const { fastLength, slowLength, source, signalLength } = signal;
-          const macd = new MovingAverageConvergenceDivergence(
-            this.stockList,
-            fastLength,
-            slowLength,
-            source,
-            signalLength,
-          );
-          this.stockList = macd.compute();
+
+          this._initSignalsWithMACD(fastLength, slowLength, source, signalLength);
           break;
         }
         default: {
@@ -100,6 +75,42 @@ export default class BackTest {
         }
       }
     });
+  }
+
+  _initSignalsWithMACD(fastLength, slowLength, source, signalLength) {
+    const macd = new MovingAverageConvergenceDivergence(
+      this.stockList,
+      fastLength,
+      slowLength,
+      source,
+      signalLength,
+    );
+
+    this.stockList = macd.compute();
+  }
+
+  _initSignalsWithEMA(periods, source, newProperty, smaSource) {
+    const ema = new ExponentialMovingAverage(
+      this.stockList,
+      periods[0],
+      source,
+      newProperty,
+      smaSource,
+    );
+
+    this.stockList = ema.compute();
+  }
+
+  _initSignalsWithSMA(periods, source, newProperty) {
+    periods.forEach(period => {
+      const sma = new SimpleMovingAverage(this.stockList, period, source, newProperty);
+      this.stockList = sma.compute();
+    });
+
+    if (periods.length >= 2) {
+      /* Always put the slow at the start of the list */
+      this.stockList = SimpleMovingAverage.identifyTrend(this.stockList, periods[0], periods[1]);
+    }
   }
 
   /**
@@ -118,64 +129,24 @@ export default class BackTest {
       let buyScore = 0;
       let sellScore = 0;
 
-      if (this.lastAction() === 'Nothing' || this.lastAction() === 'SELL') {
+      if (this.availableAction() === 'Nothing' || this.availableAction() === 'BUY') {
         buySignal.forEach(signal => {
           const currentIndex = this.stockList.indexOf(stock);
 
           if (!unIdentifiedBuySignalEncountered) {
             switch (signal.code) {
               case SIGNALS.MACD_CROSSOVER: {
-                if (currentIndex !== 0) {
-                  const previousStock = this.stockList[currentIndex - 1];
-                  const currentStock = stock;
+                buyScore = this.macdCrossOver(currentIndex, stock, signal, buyScore);
 
-                  buyScore = MovingAverageConvergenceDivergence.crossOver(
-                    previousStock,
-                    currentStock,
-                    signal.signalLength,
-                  )
-                    ? (buyScore += 1)
-                    : buyScore;
-                }
                 break;
               }
               case SIGNALS.MACD_ABOVE_SIGNAL: {
-                const { stockList } = this;
-                const currentStock = stock;
+                buyScore = this.macdAboveSignal(currentIndex, stock, signal, buyScore);
 
-                /* istanbul ignore else */
-                if (currentIndex >= signal.duration) {
-                  buyScore = MovingAverageConvergenceDivergence.aboveSignal(
-                    stockList,
-                    currentStock,
-                    signal.signalLength,
-                    signal.duration,
-                  )
-                    ? (buyScore += 1)
-                    : buyScore;
-                }
                 break;
               }
               case SIGNALS.UPTREND: {
-                const { duration } = signal;
-                let qualified = true;
-
-                if (duration) {
-                  if (currentIndex - 1 >= duration) {
-                    for (let i = duration; i >= 0; i -= 1) {
-                      if (!SimpleMovingAverage.upTrend(stock)) {
-                        qualified = false;
-                        break;
-                      }
-                    }
-                  } else {
-                    qualified = false;
-                  }
-                } else {
-                  qualified = SimpleMovingAverage.upTrend(stock);
-                }
-
-                buyScore = qualified ? (buyScore += 1) : buyScore;
+                buyScore = this.upTrend(currentIndex, stock, signal, buyScore);
                 break;
               }
               case SIGNALS.PRICE_ABOVE_EMA: {
@@ -203,7 +174,7 @@ export default class BackTest {
         }
       }
 
-      if (this.lastAction() === 'BUY') {
+      if (this.availableAction() === 'SELL') {
         sellSignal.forEach(signal => {
           const currentIndex = this.stockList.indexOf(stock);
 
@@ -212,58 +183,17 @@ export default class BackTest {
             switch (signal.code) {
               case SIGNALS.MACD_CROSSUNDER: {
                 /* istanbul ignore else */
-                if (currentIndex !== 0) {
-                  const previousStock = this.stockList[currentIndex - 1];
-                  const currentStock = stock;
-
-                  sellScore = MovingAverageConvergenceDivergence.crossUnder(
-                    previousStock,
-                    currentStock,
-                    signal.signalLength,
-                  )
-                    ? (sellScore += 1)
-                    : sellScore;
-                }
+                sellScore = this.crossUnder(currentIndex, stock, signal, sellScore);
                 break;
               }
               case SIGNALS.MACD_BELOW_SIGNAL: {
-                const { stockList } = this;
-                const currentStock = stock;
-
-                /* istanbul ignore else */
-                if (currentIndex >= signal.duration) {
-                  buyScore = MovingAverageConvergenceDivergence.belowSignal(
-                    stockList,
-                    currentStock,
-                    signal.signalLength,
-                    signal.duration,
-                  )
-                    ? (buyScore += 1)
-                    : buyScore;
-                }
+                sellScore = this.macdBelowSignal(stock, currentIndex, signal, sellScore);
 
                 break;
               }
               case SIGNALS.DOWNTREND: {
-                const { duration } = signal;
-                let qualified = true;
+                sellScore = this.downTrend(stock, currentIndex, signal, sellScore);
 
-                if (duration) {
-                  if (currentIndex - 1 >= duration) {
-                    for (let i = duration; i >= 0; i -= 1) {
-                      if (!SimpleMovingAverage.downTrend(stock)) {
-                        qualified = false;
-                        break;
-                      }
-                    }
-                  } else {
-                    qualified = false;
-                  }
-                } else {
-                  qualified = SimpleMovingAverage.downTrend(stock);
-                }
-
-                buyScore = qualified ? (buyScore += 1) : buyScore;
                 break;
               }
               case SIGNALS.PRICE_BELOW_EMA: {
@@ -286,6 +216,10 @@ export default class BackTest {
           }
         });
 
+        /**
+         * Temporary code for checking the P/L
+         * TODO: create a transastion list for this.
+         */
         if (sellSignal.length !== 0 && sellScore === sellPerfectScore) {
           const boughtStock = this.history[this.history.length - 1].stock;
           const boughtShares = this.history[this.history.length - 1].numberOfShares;
@@ -321,9 +255,133 @@ export default class BackTest {
     return this.history;
   }
 
-  lastAction() {
+  macdBelowSignal(stock, currentIndex, signal, sellScore) {
+    const { stockList } = this;
+    const currentStock = stock;
+    let score = sellScore;
+    /* istanbul ignore else */
+    if (currentIndex >= signal.duration) {
+      score = MovingAverageConvergenceDivergence.belowSignal(
+        stockList,
+        currentStock,
+        signal.signalLength,
+        signal.duration,
+      )
+        ? (score += 1)
+        : score;
+    }
+    return score;
+  }
+
+  crossUnder(currentIndex, stock, signal, sellScore) {
+    let score = sellScore;
+
+    if (currentIndex !== 0) {
+      const previousStock = this.stockList[currentIndex - 1];
+      const currentStock = stock;
+
+      score = MovingAverageConvergenceDivergence.crossUnder(
+        previousStock,
+        currentStock,
+        signal.signalLength,
+      )
+        ? (score += 1)
+        : score;
+    }
+    return score;
+  }
+
+  macdAboveSignal(currentIndex, stock, signal, buyScore) {
+    const { stockList } = this;
+    const currentStock = stock;
+    let score = buyScore;
+
+    /* istanbul ignore else */
+    if (currentIndex >= signal.duration) {
+      score = MovingAverageConvergenceDivergence.aboveSignal(
+        stockList,
+        currentStock,
+        signal.signalLength,
+        signal.duration,
+      )
+        ? (score += 1)
+        : score;
+    }
+    return score;
+  }
+
+  macdCrossOver(currentIndex, stock, signal, buyScore) {
+    let score = buyScore;
+
+    if (currentIndex !== 0) {
+      const previousStock = this.stockList[currentIndex - 1];
+      const currentStock = stock;
+      score = MovingAverageConvergenceDivergence.crossOver(
+        previousStock,
+        currentStock,
+        signal.signalLength,
+      )
+        ? (score += 1)
+        : score;
+    }
+    return score;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  upTrend(currentIndex, stock, signal, buyScore) {
+    const { duration } = signal;
+    let qualified = true;
+    let score = buyScore;
+
+    if (duration) {
+      if (currentIndex - 1 >= duration) {
+        for (let i = duration; i >= 0; i -= 1) {
+          if (!SimpleMovingAverage.upTrend(stock)) {
+            qualified = false;
+            break;
+          }
+        }
+      } else {
+        qualified = false;
+      }
+    } else {
+      qualified = SimpleMovingAverage.upTrend(stock);
+    }
+
+    score = qualified ? (score += 1) : score;
+
+    return score;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  downTrend(currentIndex, stock, signal, sellScore) {
+    const { duration } = signal;
+    let qualified = true;
+    let score = sellScore;
+
+    if (duration) {
+      if (currentIndex - 1 >= duration) {
+        for (let i = duration; i >= 0; i -= 1) {
+          if (!SimpleMovingAverage.downTrend(stock)) {
+            qualified = false;
+            break;
+          }
+        }
+      } else {
+        qualified = false;
+      }
+    } else {
+      qualified = SimpleMovingAverage.downTrend(stock);
+    }
+
+    score = qualified ? (score += 1) : score;
+
+    return score;
+  }
+
+  availableAction() {
     if (this.history.length === 0) return 'Nothing';
 
-    return this.history[this.history.length - 1].action;
+    return this.history[this.history.length - 1].action === 'BUY' ? 'SELL' : 'BUY';
   }
 }
