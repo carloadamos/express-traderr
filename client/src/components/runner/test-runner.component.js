8d8374076101bb22557783cafd777ce63e26efe9
carloadamos <johncarloadamos@gmail.com>
2020-05-29 08:41:07 +0800

    Add two dropdown for test runner

--- client/src/components/runner/test-runner.component.js
+++ client/src/components/runner/test-runner.component.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import '../../style/testrunner.style.css'
 
 export default class TestRunner extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stockList: ['ALI', 'NOW', 'BDO'],
+    }
+  }
 
   render() {
     return (
@@ -9,11 +17,59 @@ export default class TestRunner extends Component {
           <p>LABORATORY</p>
         </div>
         <div className="card">
-          FROM TO STOCK STRATEGY STOPLOSS
+          FROM TO STRATEGY STOPLOSS
+          {this._renderSelectStock()}
+          {this._renderSelectStrategy()}
         </div>
       </div>
     );
 
+  }
+  _renderSelectStock() {
+    return (
+      <div id="tsStockDd">
+        <span>Stock</span>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false">
+            ALL
+          </button>
+          <div className="dropdown-menu" aria-labelledby="strategyDropDown">
+            {this.state.stockList.map((stock, i) => {
+              return (<a key={i} className="dropdown-item" href="#!" onClick={() => console.log(`You clicked ${stock}`)}>{stock}</a>);
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  _renderSelectStrategy() {
+    return (
+      <div id="tsStratDd">
+        <span>Strategy</span>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="strategyDropDown"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false">
+            {this.state.selectedStrategy ? this.state.selectedStrategy.strategy_name : "ALL"}
+          </button>
+          <div className="dropdown-menu" aria-labelledby="strategyDropDown">
+            {this.state.stockList.map((stock, i) => {
+              return (<a key={i} className="dropdown-item" href="#!" onClick={() => console.log(`You clicked ${stock}`)}>{stock}</a>);
+            })}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
